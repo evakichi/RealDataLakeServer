ed0cb15bcf23bf73a111909bd458d961327d1bd3
Makoto ISHIHARA <evakichi@be.wakwak.com>
2025-03-02 14:55:12 +0900

    DB操作とテストコードの追加。

--- spark/py/test.py
+++ spark/py/test.py
@@ -1,6 +1,8 @@
 from  pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import DoubleType, FloatType, LongType, StructType,StructField, StringType
+
 spark = SparkSession.builder.getOrCreate()
+
 schema = StructType([
   StructField("vendor_id", LongType(), True),
   StructField("trip_id", LongType(), True),
@@ -10,4 +12,16 @@ schema = StructType([
 ])
 
 df = spark.createDataFrame([], schema)
-df.writeTo("demo.nyc.taxis").create()
+df.writeTo("demo.nyc.taxis00000").create()
+
+schema = spark.table("demo.nyc.taxis00000").schema
+data = [
+    (1, 1000371, 1.8, 15.32, "N"),
+    (2, 1000372, 2.5, 22.15, "N"),
+    (2, 1000373, 0.9, 9.01, "N"),
+    (1, 1000374, 8.4, 42.13, "Y")
+  ]
+df = spark.createDataFrame(data, schema)
+df.writeTo("demo.nyc.taxis00000").append()
+
+df = spark.table("demo.nyc.taxis00000").show()
